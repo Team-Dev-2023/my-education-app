5a903dc5263f094f3b519ab8cfb10f36a6183c67
camhoccode <nguyenchoncam1998@gmail.com>
2023-07-21 23:27:23 +0700

    Update: render result while searching courses

--- web-portal/src/components/Header/SearchOnTyping.jsx
+++ web-portal/src/components/Header/SearchOnTyping.jsx
@@ -1,15 +1,108 @@
-import React from "react";
+import axios from "axios";
+import { API_ENDPOINT } from "constants/api";
+import React, { useEffect, useRef, useState } from "react";
+import { flushSync } from "react-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 
+const api = process.env.REACT_APP_API;
+
 function SearchOnTyping() {
+  const [inputChange, setInputChange] = useState("");
+  const [searchedCourses, setSearchedCourses] = useState([]);
+
+  const searchResultDivRef = useRef(null);
+  const inputFieldDivRef = useRef(null);
+
+  const fetchSearchedCourses = async (query) => {
+    const result = await axios.get(`${api}${API_ENDPOINT.COURSES}`, {
+      params: {
+        perPage: 5,
+      },
+    });
+    const fullCourses = result.data.data;
+    return fullCourses.filter((item) =>
+      item.title.toLowerCase().includes(query.toLowerCase())
+    );
+  };
+  const handleInputChange = (event) => {
+    flushSync(() => {
+      setInputChange(event.target.value);
+    });
+  };
+
+  useEffect(() => {
+    const debounceSearching = setTimeout(() => {
+      fetchSearchedCourses(inputChange).then((result) => {
+        setSearchedCourses(result);
+        console.log("res", result);
+      });
+    }, 300);
+
+    return () => {
+      clearTimeout(debounceSearching);
+    };
+  }, [inputChange]);
+
+  // console.log("searched courses", searchedCourses);
+
+  useEffect(() => {
+    if (inputFieldDivRef.current && searchResultDivRef.current) {
+      const resizeObserver = new ResizeObserver((entries) => {
+        for (const entry of entries) {
+          const width = entry.contentRect.width;
+          searchResultDivRef.current.style.width = `${width}px`;
+        }
+      });
+
+      resizeObserver.observe(inputFieldDivRef.current);
+
+      return () => {
+        resizeObserver.unobserve(inputFieldDivRef.current);
+      };
+    }
+  }, []);
+
   return (
-    <div className="inline-flex p-2 items-center rounded-full border-2 border-[#666163]">
-      <AiOutlineSearch className="ml-1 opacity-80 text-[30px]" />
-      <input
-        type="text"
-        placeholder="Search for anything"
-        className="pl-1  w-full rounded-full border-none focus:outline-none"
-      />
+    <div className="relative flex flex-col items-start">
+      <div
+        className="inline-flex w-full p-2 items-center rounded-full border-2 border-[#666163]"
+        ref={inputFieldDivRef}
+      >
+        <AiOutlineSearch className="ml-1 opacity-80 text-[30px]" />
+        <input
+          type="text"
+          value={inputChange}
+          onChange={handleInputChange}
+          placeholder="Search for anything"
+          className="pl-1  w-full rounded-full border-none focus:outline-none"
+        />
+      </div>
+      {searchedCourses.length > 0 && inputChange.length > 0 && (
+        <div
+          className="absolute top-[50px] flex flex-col bg-[#fff] mt-[4px] py-[8px] border border-[#d1d7dc] z-[1000] text-ellipsis whitespace-nowrap overflow-hidden"
+          ref={searchResultDivRef}
+        >
+          {searchedCourses.map((item) => (
+            <a
+              key={item.uuid}
+              className="flex flex-row pl-[12px] pr-[8px] py-[8px] cursor-pointer"
+              href={`/detail/${item.uuid}`}
+            >
+              <div className="w-[32px] h-[32px] mr-[16px] flex justify-center">
+                <img src={item.imageUrl} alt="course" className="h-full" />
+              </div>
+              <div className="flex flex-col">
+                <div className="text-[16px] font-[700] leading-[1.2] text-ellipsis whitespace-nowrap">
+                  {item.title}
+                </div>
+                <div className="text-[12px] font-[400] leading-[1.4]">
+                  Created by: {item.createdBy}
+                </div>
+              </div>
+            </a>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
